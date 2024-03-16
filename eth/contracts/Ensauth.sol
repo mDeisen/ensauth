2fbb6f38ca1153e3e6752fa9a511bd1ffedce8a3
Mattis Deisen <mattis@deisen.net>
2024-03-16 08:31:11 +0000

    Update contract function signatures

--- eth/contracts/Ensauth.sol
+++ eth/contracts/Ensauth.sol
@@ -25,53 +25,86 @@ contract Ensauth is IERC1155Receiver, ERC165 {
 
     mapping(bytes32 => Application) private applications;
 
-    function registerApplication(
-        bytes32 node,
-        address /* ensAddress */
-    ) public {
-        require(!applications[node].exists, "Application already registered.");
+    /**
+     * 
+     * @param groupnode -- the namehash of the subdomain starting with "groups"
+     * Check if the application exists. Revert if it does.
+     * Set the resolver for the subdomain to the public ens resolver
+     * Set the ens lookup address of the subdomain to the address of this contract
+     */
+    function registerApplication(bytes32 groupnode) public {
+        
+        // Check if the application exists. Revert if it does
+        require(!applications[groupnode].exists, "Application already registered.");
 
-        // Simply setting the existence flag is enough to "create" the application
-        applications[node].exists = true;
+        // Add the application entry
+        applications[groupnode].exists = true;
 
-        // Set the address in the resolver (currently hardcoded to the public resolver address on mainnet)
+        // Set the address in the resolver (currently hardcoded to the public resolver)
         ENS ens;
-        ens.setResolver(node, 0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63);
-
+        ens.setResolver(groupnode, 0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63);
+        
+        // Set the ens lookup address of the subdomain to the address of this contract
         Resolver resolver;
-        resolver.setAddr(node, address(this));
+        resolver.setAddr(groupnode, address(this));
     }
 
-    function registerRole(bytes32 app, string memory roleName) public {
-        require(applications[app].exists, "Application does not exist.");
-        require(
-            !applications[app].roles[roleName].exists,
-            "Role already exists."
-        );
-        applications[app].roles[roleName].exists = true;
+    /**
+     * @param groupnode -- the namehash of the subdomain
+     * @param roleName -- the name of the role
+     * Revert if the application does not exists.
+     * Revert if the role exists.
+     * Add the role to the application
+     */
+    function registerRole(bytes32 groupnode, string memory roleName) public {
+        require(applications[groupnode].exists, "Application does not exist.");
+        require(!applications[groupnode].roles[roleName].exists,"Role already exists.");
+        applications[groupnode].roles[roleName].exists = true;
     }
 
+    /**
+     * @param groupnode -- the namehash of the subdomain
+     * @param roleName -- the name of the role
+     * @param account -- the address of the account
+     * Revert if the application does not exists.
+     * Add the account to the role
+     */
     function assignRole(
-        bytes32 app,
+        bytes32 groupnode,
         string memory roleName,
         address account
     ) public {
-        require(applications[app].exists, "Application does not exist.");
-        applications[app].roles[roleName].users.push(account);
+        // Revert if the application does not existst
+        require(applications[groupnode].exists, "Application does not exist.");
+
+        // Revert if the role does not exist
+        require(applications[groupnode].roles[roleName].exists,"Role does not exist.");
+
+        // Add the account to the role
+        applications[groupnode].roles[roleName].users.push(account);
     }
 
+    /**
+     * @param groupnode -- the namehash of the subdomain
+     * @param roleName -- the name of the role
+     * @param account -- the address of the account
+     * Revert if the application does not exists.
+     * Revert if the role does not exists.
+     * Remove the account from the role
+     */
     function removeRole(
-        bytes32 app,
+        bytes32 groupnode,
         string memory roleName,
         address account
     ) public {
-        require(applications[app].exists, "Application does not exist.");
-        require(
-            applications[app].roles[roleName].exists,
-            "Role does not exist."
-        );
+        // Revert if the application does not exist
+        require(applications[groupnode].exists, "Application does not exist.");
 
-        Role storage role = applications[app].roles[roleName];
+        // Revert if the role does not exist
+        require(applications[groupnode].roles[roleName].exists,"Role does not exist.");
+
+        // Remove the account from the role
+        Role storage role = applications[groupnode].roles[roleName];
         for (uint256 i = 0; i < role.users.length; i++) {
             if (role.users[i] == account) {
                 role.users[i] = role.users[role.users.length - 1];
@@ -107,11 +140,11 @@ contract Ensauth is IERC1155Receiver, ERC165 {
      * If the subdomain starts with "groups" & the owner of the parent domain sent the token, the application will be registered.
      */
     function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 value,
-        bytes calldata data // this should be namehash of parent domain
+        address operator, // The address which initiated the transfer
+        address from, // The address which previously owned the token
+        uint256 id, // The labelhash of the .eth domain
+        uint256 value, // ??
+        bytes calldata data // ?? 
     ) external override returns (bytes4) {
         (
             string memory ensSubdomain,
@@ -125,7 +158,7 @@ contract Ensauth is IERC1155Receiver, ERC165 {
             "Subdomain must be 'groups'"
         );
 
-        registerApplication(bytes32(id), from);
+        registerApplication(bytes32(id));
 
         return this.onERC1155Received.selector;
     }
@@ -135,26 +168,21 @@ contract Ensauth is IERC1155Receiver, ERC165 {
      * For each token in the batch register the application if the subdomain starts with "groups"
      */
     function onERC1155BatchReceived(
-        address operator,
-        address from,
-        uint256[] calldata ids,
-        uint256[] calldata values,
+        address operator, // The address which initiated the batch
+        address from, // Registrant address
+        uint256[] calldata ids, //labelhashes of the .eth domains
+        uint256[] calldata values, // empty
         bytes calldata data
     ) external override returns (bytes4) {
         for (uint256 i = 0; i < ids.length; i++) {
-            (
-                string memory ensSubdomain,
-                string memory ensParentDomain
+            (string memory ensSubdomain, string memory ensParentDomain
             ) = extractEnsDomainsFromTokenData(ids[i], data);
 
             // If the subdomain does not start with "groups" reject the token
-            require(
-                keccak256(abi.encodePacked(ensSubdomain)) ==
-                    keccak256(abi.encodePacked("groups")),
-                "Subdomain must be 'groups'"
-            );
+            require(keccak256(abi.encodePacked(ensSubdomain)) ==
+                    keccak256(abi.encodePacked("groups")),"Subdomain must be 'groups'");
 
-            registerApplication(bytes32(ids[i]), from);
+            registerApplication(bytes32(ids[i]));
         }
         return this.onERC1155BatchReceived.selector;
     }
@@ -169,7 +197,6 @@ contract Ensauth is IERC1155Receiver, ERC165 {
         uint256 id,
         bytes memory data
     ) private pure returns (string memory, string memory) {
-        // Example implementation. You'll need to replace this with actual logic to extract subdomain and parent domain
         string memory ensSubdomain = "groups";
         string memory ensParentDomain = "example.eth";
 
