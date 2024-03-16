c994723da4bd98057884d95fa52e9cb9629be49f
Mattis Deisen <mattis@deisen.net>
2024-03-16 02:37:28 +0000

    Configure sepolia for deployment

--- eth/hardhat.config.ts
+++ eth/hardhat.config.ts
@@ -1,6 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+require('dotenv').config(); 
+require("hardhat-ens-mock");
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `${process.env.ALCHEMY_SEPOLIA_URL}`,
+      accounts: [`0x${process.env.SEPOLIA_PRIVATE_KEY}`],
+    }
+  }
+};
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
 };
