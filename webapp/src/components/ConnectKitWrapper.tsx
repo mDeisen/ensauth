7fbdd9cb5091d260272ea5630b9ea188c70b74a5
Stephan Wittig <stephan.wittig@cgi.com>
2024-03-16 23:34:44 +0100

    add mainnet

--- webapp/src/components/ConnectKitWrapper.tsx
+++ webapp/src/components/ConnectKitWrapper.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { sepolia } from "wagmi/chains";
+import { sepolia, mainnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { FC } from "react"
@@ -15,12 +15,15 @@ console.table({
 const config = createConfig(
   getDefaultConfig({
     // Your dApps chains
-    chains: [addEnsContracts(sepolia)],
+    chains: [addEnsContracts(sepolia), addEnsContracts(mainnet)],
     transports: {
       // RPC URL for each chain
       [sepolia.id]: http(
         `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
       ),
+      [mainnet.id]: http(
+        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`
+      )
     },
 
     // Required API Keys
